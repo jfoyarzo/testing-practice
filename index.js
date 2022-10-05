ad66be436362d4b97fb29a9dfe9db0a21864622f
jfoyarzo <foyarzo621@gmail.com>
2022-10-05 14:56:04 -0300

    Add tests corresponding to Task 1 of the excercise

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-const stringLength = string => string.length;
+const stringLength = string => {
+    if (string.length >= 1 && string.length <= 10) {
+        return string.length;
+    } else {
+        throw new Error('The string must have between 1 and 10 characters');
+    }
+}
 
 module.exports = stringLength;
--- index.test.js
+++ index.test.js
@@ -1,5 +1,13 @@
-const stringLength = require('./index')
+const stringLength = require('./index');
 
 test('Return 5 for the word hello', () => {
-    expect(stringLength('hello')).toBe(5)
+    expect(stringLength('hello')).toBe(5);
+})
+
+test('Throw error for an empty string', () => {
+    expect(() => stringLength('')).toThrow('The string must have between 1 and 10 characters');
+})
+
+test('Throw error for an 12 character string', () => {
+    expect(() => stringLength('shoppingList')).toThrow('The string must have between 1 and 10 characters')
 })
